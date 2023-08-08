337cca08c4a51163ac9ad61cfa09e804f6115989
carlosmoshudev <carlos@carlosmoshu.com>
2023-08-08 12:57:37 +0200

    main page and layout

---
+++ frontend/svelte-typescript/src/routes/+layout.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    const pages = [
+        { name: 'Inicio', path: '/' },
+        { name: 'Ver Contactos', path: '/contact-view' },
+        { name: 'Añadir Contacto', path: '/add-contact' }
+    ];
+</script>
+
+<nav>
+    <ul>
+        {#each pages as page}
+            <li>
+                <a href="{page.path}">{page.name}</a>
+            </li>
+        {/each}
+    </ul>
+</nav>
+
+<main>
+    <slot></slot>
+</main>
+
+<style lang="css" scoped>
+    nav {
+        background-image: linear-gradient(to right, #3a1f00, #390719);
+        color: #fff;
+        padding: 1em;
+        border-radius: 0.5em;
+        filter: drop-shadow(0em 0.4em 0.3em #33333385);
+    }
+
+    nav ul {
+        display: flex;
+        justify-content: space-between;
+        list-style: none;
+        padding: 0 1em;
+        margin: 0;
+    }
+
+    nav ul li {
+        padding: 0.2em 0.5em;
+    }
+
+    nav ul li a {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1em;
+        color: #ffd9d9ee;
+        text-decoration: none;
+    }
+
+    nav ul li a:hover {
+        text-decoration: underline;
+    }
+
+    main {
+        padding: 1em;
+    }
+</style>
--- frontend/svelte-typescript/src/routes/+page.svelte
+++ frontend/svelte-typescript/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    // no-script
+</script>
+
+<article id="inicio">
+    <h1>Inicio</h1>
+    <p>Esta es la página de inicio</p>
+</article>
+
+<style lang="css" scoped>
+
+    article {
+        padding: 1em;
+    }
+
+    article#inicio {
+        background-image: linear-gradient(to right, #d79e5dd7, #b53765d7);
+        color: #a7d129;
+        border-radius: 0.5em;
+        filter: drop-shadow(0em 0.4em 0.3em #312f075b);
+        min-height: 75vh;
+    }
+
+    article#inicio h1 {
+        font-family: 'Roboto', sans-serif;
+        font-size: 2.5em;
+        color: #391313;
+    }
+
+    article#inicio p {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1.5em;
+        color: #2d0606ee;
+    }
+</style>
